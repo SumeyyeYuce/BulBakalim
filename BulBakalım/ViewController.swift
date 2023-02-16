80d62c8173bb76e8d48cb0e2fc41f95eeda47829
SumeyyeYuce <yucesumeyye25@gmail.com>
2023-02-16 13:33:20 +0300

    Initial Commit

---
+++ BulBakalım/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BulBakalım
+//
+//  Created by Sümeyye on 16.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
